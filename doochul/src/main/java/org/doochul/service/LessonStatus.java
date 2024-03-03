ef24c7d116986d2c81afb717765db19a7d451ad7
yuseonjun <seonjun0906@gmail.com>
2024-03-03 16:49:12 +0900

    feat: 오후 **시 **분 형식으로 변경하는 메서드 구현

--- doochul/src/main/java/org/doochul/service/LessonStatus.java
+++ doochul/src/main/java/org/doochul/service/LessonStatus.java
@@ -1,6 +1,7 @@
 package org.doochul.service;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @FunctionalInterface
 interface LessonStatusMessage {
@@ -8,10 +9,13 @@ interface LessonStatusMessage {
 }
 
 public enum LessonStatus {
-    BEFORE_LESSON("수업 전", (name, time, teacher) -> name + "님 오전 " + time + " " + teacher + " 강사님 수업 잊지 않으셨죠?"),
+    BEFORE_LESSON("수업 전",
+            (name, time, teacher) -> name + "님 " + formatToLocalTime(time) + " " + teacher + " 강사님 수업 잊지 않으셨죠?"),
     AFTER_LESSON("수업 후", (name, time, teacher) -> name + "님 오늘 수업 잘 받으셨나요?"),
-    SCHEDULED_LESSON("수업 신청", (name, time, teacher) -> "오전 " + time + " " + name + "님 " + teacher + " 강사님 수업을 신청했습니다."),
-    WITHDRAWN_LESSON("수업 철회", (name, time, teacher) -> "오전 " + time + " " + name + "님 " + teacher + " 강사님의 수업을 철회했습니다."),
+    SCHEDULED_LESSON("수업 신청",
+            (name, time, teacher) -> formatToLocalTime(time) + " " + name + "님 " + teacher + " 강사님 수업을 신청했습니다."),
+    WITHDRAWN_LESSON("수업 철회",
+            (name, time, teacher) -> formatToLocalTime(time) + " " + name + "님 " + teacher + " 강사님의 수업을 철회했습니다."),
     END_LESSON("수업 종료", (name, time, teacher) -> teacher + " 강사님의 수업이 모두 종료되었습니다.");
 
     private final String title;
@@ -20,6 +24,10 @@ public enum LessonStatus {
     LessonStatus(String title, LessonStatusMessage message) {
         this.title = title;
         this.message = message;
+    }
+
+    private static String formatToLocalTime(LocalDateTime time) {
+        return time.format(DateTimeFormatter.ofPattern("a HH시 mm분"));
     }
 
     public String getTitle() {

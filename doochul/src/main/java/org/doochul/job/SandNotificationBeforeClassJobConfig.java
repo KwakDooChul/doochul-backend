3268caf0fb2661149cea444c29516539ffc75b97
ingpyo <juninpyo726@naver.com>
2024-03-10 10:41:35 +0900

    feat: Batch를 사용해 수업전 리마인드 알람 스케줄 작성

---
+++ doochul/src/main/java/org/doochul/job/SandNotificationBeforeClassJobConfig.java
@@ -0,0 +1,98 @@
+package org.doochul.job;
+
+import org.doochul.service.Letter;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.jdbc.core.ColumnMapRowMapper;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.doochul.service.LessonStatus.BEFORE_LESSON;
+
+@Configuration
+public class SandNotificationBeforeClassJobConfig {
+    private static final int CHUNK_SIZE = 10;
+
+    private final DataSource dataSource;
+    private final SendNotificationItemWriter sendNotificationItemWriter;
+
+    public SandNotificationBeforeClassJobConfig(final DataSource dataSource, final SendNotificationItemWriter sendNotificationItemWriter) {
+        this.dataSource = dataSource;
+        this.sendNotificationItemWriter = sendNotificationItemWriter;
+    }
+
+    @Bean
+    public Job sendNotificationBeforeClassJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) throws Exception {
+        return new JobBuilder("sendNotificationBeforeClassJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .start(sendNotificationBeforeClassStep(jobRepository, transactionManager))
+                .build();
+    }
+
+    @Bean
+    public Step sendNotificationBeforeClassStep(
+            JobRepository jobRepository,
+            PlatformTransactionManager transactionManager
+    ) throws Exception {
+        return new StepBuilder("addNotificationStep", jobRepository)
+                .<Map<String, Object>, Letter>chunk(CHUNK_SIZE, transactionManager)
+                .reader(addNotificationItemReader())
+                .processor(addNotificationItemProcessor())
+                .writer(sendNotificationItemWriter)
+                .taskExecutor(new SimpleAsyncTaskExecutor())
+                .build();
+    }
+
+    @Bean
+    public JdbcPagingItemReader<Map<String, Object>> addNotificationItemReader() throws Exception {
+        Map<String, Object> parameterValues = new HashMap<>();
+        parameterValues.put("startedAt", LocalDateTime.now().plusMinutes(10));
+
+        return new JdbcPagingItemReaderBuilder<Map<String, Object>>()
+                .name("addNotificationItemReader")
+                .dataSource(dataSource)
+                .pageSize(CHUNK_SIZE)
+                .queryProvider(createQueryProvider())
+                .parameterValues(parameterValues)
+                .rowMapper(new ColumnMapRowMapper())
+                .build();
+    }
+
+    private PagingQueryProvider createQueryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+        queryProvider.setDataSource(dataSource);
+        queryProvider.setSelectClause("u.device_token AS student_token, u.name AS student_name, t.name AS teacher_name, l.started_at");
+        queryProvider.setFromClause("FROM lesson l JOIN users u ON l.student_id = u.id JOIN users t ON l.teacher_id = t.id");
+        queryProvider.setWhereClause("WHERE l.started_at <= :startedAt");
+
+        return queryProvider.getObject();
+    }
+
+    @Bean
+    public ItemProcessor<Map<String, Object>, Letter> addNotificationItemProcessor() {
+        return item -> {
+            String studentToken = (String) item.get("student_token");
+            String studentName = (String) item.get("student_name");
+            String teacherName = (String) item.get("teacher_name");
+            LocalDateTime startedAt = (LocalDateTime) item.get("started_at");
+
+            return Letter.of(studentToken, studentName, teacherName, startedAt, BEFORE_LESSON);
+        };
+    }
+}

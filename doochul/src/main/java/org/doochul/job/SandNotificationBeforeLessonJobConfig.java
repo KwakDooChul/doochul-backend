e82ad1627624bd09a04c535bb7d4aecc75c24285
ingpyo <juninpyo726@naver.com>
2024-03-10 19:41:16 +0900

    feat: Batch를 사용해 수업전 리마인드 알람 스케줄 작성(JPA -> 쿼리로 변경)

--- doochul/src/main/java/org/doochul/job/SandNotificationBeforeLessonJobConfig.java
+++ doochul/src/main/java/org/doochul/job/SandNotificationBeforeLessonJobConfig.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
 import org.springframework.batch.item.database.PagingQueryProvider;
 import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
 import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
@@ -22,31 +23,32 @@ import javax.sql.DataSource;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 import static org.doochul.service.LessonStatus.BEFORE_LESSON;
 
 @Configuration
-public class SandNotificationBeforeClassJobConfig {
+public class SandNotificationBeforeLessonJobConfig {
     private static final int CHUNK_SIZE = 10;
 
     private final DataSource dataSource;
     private final SendNotificationItemWriter sendNotificationItemWriter;
 
-    public SandNotificationBeforeClassJobConfig(final DataSource dataSource, final SendNotificationItemWriter sendNotificationItemWriter) {
+    public SandNotificationBeforeLessonJobConfig(final DataSource dataSource, final SendNotificationItemWriter sendNotificationItemWriter) {
         this.dataSource = dataSource;
         this.sendNotificationItemWriter = sendNotificationItemWriter;
     }
 
     @Bean
-    public Job sendNotificationBeforeClassJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) throws Exception {
-        return new JobBuilder("sendNotificationBeforeClassJob", jobRepository)
+    public Job sandNotificationBeforeLessonJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) throws Exception {
+        return new JobBuilder("sandNotificationBeforeLessonJob", jobRepository)
                 .incrementer(new RunIdIncrementer())
-                .start(sendNotificationBeforeClassStep(jobRepository, transactionManager))
+                .start(sendNotificationBeforeLessonStep(jobRepository, transactionManager))
                 .build();
     }
 
     @Bean
-    public Step sendNotificationBeforeClassStep(
+    public Step sendNotificationBeforeLessonStep(
             JobRepository jobRepository,
             PlatformTransactionManager transactionManager
     ) throws Exception {
@@ -77,11 +79,14 @@ public class SandNotificationBeforeClassJobConfig {
     private PagingQueryProvider createQueryProvider() throws Exception {
         SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
         queryProvider.setDataSource(dataSource);
-        queryProvider.setSelectClause("u.device_token AS student_token, u.name AS student_name, t.name AS teacher_name, l.started_at");
+        queryProvider.setSelectClause("SELECT u.device_token AS student_token, u.name AS student_name, t.name AS teacher_name, l.started_at");
         queryProvider.setFromClause("FROM lesson l JOIN users u ON l.student_id = u.id JOIN users t ON l.teacher_id = t.id");
         queryProvider.setWhereClause("WHERE l.started_at <= :startedAt");
+        Map<String, Order> sortKeys = new HashMap<>(1);
+        sortKeys.put("l.started_at", Order.ASCENDING);
 
-        return queryProvider.getObject();
+        queryProvider.setSortKeys(sortKeys);
+        return Optional.ofNullable(queryProvider.getObject()).orElseThrow(IllegalArgumentException::new);
     }
 
     @Bean
@@ -91,7 +96,6 @@ public class SandNotificationBeforeClassJobConfig {
             String studentName = (String) item.get("student_name");
             String teacherName = (String) item.get("teacher_name");
             LocalDateTime startedAt = (LocalDateTime) item.get("started_at");
-
             return Letter.of(studentToken, studentName, teacherName, startedAt, BEFORE_LESSON);
         };
     }

c355f71b48df87f42fe0401e11240475d09b36cb
yukudaa <leeyookoo@naver.com>
2024-03-18 17:54:14 +0900

    feat/16-create-lesson

--- doochul/src/main/java/org/doochul/domain/product/ProductRepository.java
+++ doochul/src/main/java/org/doochul/domain/product/ProductRepository.java
@@ -1,10 +1,13 @@
 package org.doochul.domain.product;
 
 import java.util.List;
+
+import org.doochul.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAll();
+    List<Product> findByUserId(Long userId);
 }

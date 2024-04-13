204e49c65f6609c9655f24b66e0f3ae0af1f5f72
yukudaa <leeyookoo@naver.com>
2024-04-13 13:50:59 +0900

    feat: Lesson 업데이트

--- doochul/src/main/java/org/doochul/application/LessonService.java
+++ doochul/src/main/java/org/doochul/application/LessonService.java
@@ -5,11 +5,16 @@ import org.doochul.domain.lesson.Lesson;
 import org.doochul.domain.lesson.LessonRepository;
 import org.doochul.domain.membership.MemberShip;
 import org.doochul.domain.membership.MemberShipRepository;
+import org.doochul.domain.product.Product;
 import org.doochul.domain.user.User;
 import org.doochul.domain.user.UserRepository;
 import org.doochul.ui.dto.LessonRequest;
+import org.doochul.ui.dto.LessonResponse;
+import org.doochul.ui.dto.ProductResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +29,13 @@ public class LessonService {
         final User user = userRepository.findById(userId).orElseThrow();
         final MemberShip memberShip = memberShipRepository.findById(membershipId).orElseThrow();
 
-        return lessonRepository.save(Lesson.of(user, memberShip.getProduct().getTeacher(),memberShip, lessonRequest.startedAt(), lessonRequest.endedAt(), lessonRequest.record())).getId();
+        return lessonRepository.save(Lesson.of(user, memberShip.getProduct().getTeacher(), memberShip, lessonRequest.startedAt(), lessonRequest.endedAt(), lessonRequest.record())).getId();
     }
+
+    @Transactional(readOnly = true)
+    public List<LessonResponse> findByLessons() {
+        final List<Lesson> lessons = lessonRepository.findAll();
+        return LessonResponse.to(lessons);
+    }
+
 }
--- doochul/src/main/java/org/doochul/ui/LessonController.java
+++ doochul/src/main/java/org/doochul/ui/LessonController.java
@@ -4,7 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.doochul.application.LessonService;
 import org.doochul.resolver.AuthenticationPrincipal;
 import org.doochul.ui.dto.LessonRequest;
+import org.doochul.ui.dto.LessonResponse;
+import org.doochul.ui.dto.ProductResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,5 +20,11 @@ public class LessonController {
     @PostMapping("/save/{membershipId}")
     public Long save(@AuthenticationPrincipal Long userId, @PathVariable final Long membershipId, @RequestBody final LessonRequest lessonRequest) {
         return lessonService.save(userId, membershipId, lessonRequest);
+    }
+
+    @GetMapping("/lessons")
+    public ResponseEntity<List<LessonResponse>> findLessons() {
+        final List<LessonResponse> response = lessonService.findByLessons();
+        return ResponseEntity.ok(response);
     }
 }

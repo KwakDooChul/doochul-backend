c0df77e7c6a44c1fd138928e4e31ee12a99bf872
yukudaa <leeyookoo@naver.com>
2024-05-09 14:42:46 +0900

    feat: lesson 업데이트

--- doochul/src/test/java/org/doochul/service/LessonServiceTest.java
+++ doochul/src/test/java/org/doochul/service/LessonServiceTest.java
@@ -1,7 +1,6 @@
 package org.doochul.service;
 
 import org.doochul.application.RedisService;
-import org.doochul.domain.membership.MemberShipRepository;
 import org.doochul.domain.product.Product;
 import org.doochul.domain.product.ProductRepository;
 import org.doochul.domain.product.ProductType;
@@ -11,7 +10,6 @@ import org.doochul.ui.dto.ProductRegisterRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class LessonServiceTest {
@@ -22,11 +20,8 @@ public class LessonServiceTest {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private RedisService redisService;
-
     @Test
-    void lessonService_save() throws InterruptedException {
+    void lessonService_save() {
         // Given
         User user = User.of("123", "1234");
         User teacher = User.of("1234", "123");

ec72e626510a4470a95d3b1761cedcba55a2caf6
SeonJuuuun <110665247+SeonJuuuun@users.noreply.github.com>
2024-05-11 16:15:38 +0900

    [Feat] PT 한개 조회, PT 삭제 기능 추가 (#31)
    * feat: product 지우는 메서드 구현
    
    * feat: product 1개 조회 메서드 구현
    
    * feat: product 서비스 테스트 코드 구현
    
    * refactor: PostMapping으로 변경
    
    * refactor:api 변경
    
    * refactor: 멱등성을 고려하여 변경

--- doochul/src/main/java/org/doochul/ui/ProductController.java
+++ doochul/src/main/java/org/doochul/ui/ProductController.java
@@ -8,7 +8,9 @@ import org.doochul.resolver.AuthenticationPrincipal;
 import org.doochul.ui.dto.ProductRegisterRequest;
 import org.doochul.ui.dto.ProductResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,14 +24,27 @@ public class ProductController {
     @PostMapping("/product/register")
     public ResponseEntity<Long> save(
             @AuthenticationPrincipal final Long userId,
-            @RequestBody final ProductRegisterRequest productRegisterRequest) {
+            @RequestBody final ProductRegisterRequest productRegisterRequest
+    ) {
         final Long productId = productService.save(userId, productRegisterRequest);
         return ResponseEntity.created(URI.create("/product/" + productId)).build();
     }
 
-    @GetMapping("/products")
-    public ResponseEntity<List<ProductResponse>> findProducts() {
-        final List<ProductResponse> response = productService.findByProducts();
+    @GetMapping("/products/{productId}")
+    public ResponseEntity<ProductResponse> findProduct(@PathVariable final Long productId) {
+        final ProductResponse response = productService.findProduct(productId);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/products/find")
+    public ResponseEntity<List<ProductResponse>> findProducts() {
+        final List<ProductResponse> response = productService.findProducts();
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/product/{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable final Long productId) {
+        productService.deleteProduct(productId);
+        return ResponseEntity.noContent().build();
     }
 }

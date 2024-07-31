0618dace08f137bc1a441ed8970ec237b4344390
yuseonjun <seonjun0906@gmail.com>
2024-07-31 18:45:06 +0900

    modify: 예외 수정

--- doochul/src/main/java/org/doochul/common/exception/ErrorCodes.java
+++ doochul/src/main/java/org/doochul/common/exception/ErrorCodes.java
@@ -2,8 +2,12 @@ package org.doochul.common.exception;
 
 public enum ErrorCodes {
 
+    // 토큰 관련
     TOKEN_NULL_EXCEPTION("토큰이 비어있으면 안됩니다.", 1000L),
     TOKEN_BEARER_TYPE_EXCEPTION("토큰에 BEARER이 빠져있습니다.", 1001L),
+
+    // 유저 관련
+    USER_NOT_FOUND("올바르지 않은 유저 입니다.", 2000L),
 
     BAD_REQUEST("BAD_REQUEST", 9404L),
     BAD_REQUEST_JSON_PARSE_ERROR("[BAD_REQUEST] JSON_PARSE_ERROR - 올바른 JSON 형식이 아님", 9405L),

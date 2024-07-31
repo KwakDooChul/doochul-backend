a76058107fb4e9f7ffec71482d016de14264728a
yuseonjun <seonjun0906@gmail.com>
2024-07-31 19:08:58 +0900

    [Feat] PT 관련 ErrorCode 추가

--- doochul/src/main/java/org/doochul/common/exception/ErrorCodes.java
+++ doochul/src/main/java/org/doochul/common/exception/ErrorCodes.java
@@ -9,6 +9,9 @@ public enum ErrorCodes {
     // 유저 관련
     USER_NOT_FOUND("올바르지 않은 유저 입니다.", 2000L),
 
+    // PT 관련
+    PRODUCT_NOT_FOUND("올바르지 않은 PT 입니다.", 3000L),
+
     BAD_REQUEST("BAD_REQUEST", 9404L),
     BAD_REQUEST_JSON_PARSE_ERROR("[BAD_REQUEST] JSON_PARSE_ERROR - 올바른 JSON 형식이 아님", 9405L),
     INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", 9999L);

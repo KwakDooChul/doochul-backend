08c4639fe0a1c356f511e5244ef9fb4283c0c7ea
yuseonjun <seonjun0906@gmail.com>
2024-08-05 17:01:54 +0900

    [Feat] 멤버쉽 삭제, 단일 조회, 전체 조회 기능 구현

--- doochul/src/main/java/org/doochul/common/exception/ErrorCodes.java
+++ doochul/src/main/java/org/doochul/common/exception/ErrorCodes.java
@@ -12,6 +12,9 @@ public enum ErrorCodes {
     // PT 관련
     PRODUCT_NOT_FOUND("올바르지 않은 PT 입니다.", 3000L),
 
+    // 멤버쉽 관련
+    MEMBERSHIP_NOT_FOUND("올바르지 않은 멤버쉽입니다.", 4000L),
+
     BAD_REQUEST("BAD_REQUEST", 9404L),
     BAD_REQUEST_JSON_PARSE_ERROR("[BAD_REQUEST] JSON_PARSE_ERROR - 올바른 JSON 형식이 아님", 9405L),
     INTERNAL_SERVER_ERROR("INTERNAL_SERVER_ERROR", 9999L);
--- doochul/src/main/java/org/doochul/ui/MemberShipController.java
+++ doochul/src/main/java/org/doochul/ui/MemberShipController.java
@@ -1,10 +1,14 @@
 package org.doochul.ui;
 
 import java.net.URI;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.doochul.application.MemberShipService;
 import org.doochul.common.resolver.AuthenticationPrincipal;
+import org.doochul.ui.dto.MemberShipReadResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +23,27 @@ public class MemberShipController {
     public ResponseEntity<Long> createMemberShip(@AuthenticationPrincipal final Long userId,
                                                  @PathVariable final Long productId) {
         final Long memberShipId = memberShipService.createMemberShip(userId, productId);
-        return ResponseEntity.created(URI.create("/memberShips" + memberShipId)).build();
+        return ResponseEntity.created(URI.create("/memberShips/" + memberShipId)).build();
+    }
+
+    @GetMapping("/memberShip/{productId}")
+    public ResponseEntity<MemberShipReadResponse> readMemberShip(
+            @AuthenticationPrincipal final Long userId,
+            @PathVariable final Long productId
+    ) {
+        final MemberShipReadResponse readResponse = memberShipService.findMemberShip(userId, productId);
+        return ResponseEntity.ok(readResponse);
+    }
+
+    @GetMapping("/memberShips")
+    public ResponseEntity<List<MemberShipReadResponse>> readMemberShips(@AuthenticationPrincipal final Long userId) {
+        final List<MemberShipReadResponse> readResponses = memberShipService.findMemberShips(userId);
+        return ResponseEntity.ok(readResponses);
+    }
+
+    @DeleteMapping("/memberShip/{memberShipId}")
+    public ResponseEntity<Void> deleteMemberShip(@PathVariable final Long memberShipId) {
+        memberShipService.deleteMemberShip(memberShipId);
+        return ResponseEntity.noContent().build();
     }
 }

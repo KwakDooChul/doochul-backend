d45a6a35e291c6798ed2e9dd3de61eb2b4de7931
SeonJuuuun <110665247+SeonJuuuun@users.noreply.github.com>
2024-08-06 17:59:15 +0900

    [Feat] 레슨 단일 조회, 전체 조회, 헤더 -> 쿠키로 변경 (#41)
    * [Feat] Lesson 단일 조회, 전체 조회 기능 구현
    
    * [Refactor] : 헤더 -> 쿠키로 변경

--- doochul/src/main/java/org/doochul/common/exception/ErrorCodes.java
+++ doochul/src/main/java/org/doochul/common/exception/ErrorCodes.java
@@ -4,7 +4,6 @@ public enum ErrorCodes {
 
     // 토큰 관련
     TOKEN_NULL_EXCEPTION("토큰이 비어있으면 안됩니다.", 1000L),
-    TOKEN_BEARER_TYPE_EXCEPTION("토큰에 BEARER이 빠져있습니다.", 1001L),
 
     // 유저 관련
     USER_NOT_FOUND("올바르지 않은 유저 입니다.", 2000L),
@@ -18,6 +17,10 @@ public enum ErrorCodes {
    
     // 멤버쉽 관련
     MEMBERSHIP_NOT_FOUND("올바르지 않은 멤버쉽입니다.", 4000L),
+
+    // 레슨 관련
+    LESSON_NOT_FOUND("올바르지 않은 수업 입니다.", 5000L),
+    LESSON_VERIFY_OWNER("본인의 레슨이 아닙니다.", 5001L),
 
     BAD_REQUEST("BAD_REQUEST", 9404L),
     BAD_REQUEST_JSON_PARSE_ERROR("[BAD_REQUEST] JSON_PARSE_ERROR - 올바른 JSON 형식이 아님", 9405L),
--- doochul/src/main/java/org/doochul/common/interceptor/AuthorizationExtractor.java
+++ doochul/src/main/java/org/doochul/common/interceptor/AuthorizationExtractor.java
@@ -1,32 +1,39 @@
 package org.doochul.common.interceptor;
 
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.doochul.common.exception.BackEndApplicationException;
 import org.doochul.common.exception.ErrorCodes;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AuthorizationExtractor {
 
-    private static final String BEARER_TYPE = "Bearer ";
+    private static final String AUTH_COOKIE_NAME = "Authorization";
 
     public static String extract(final HttpServletRequest request) {
-        final String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if (Objects.isNull(authorizationHeader)) {
+        final Cookie[] cookies = request.getCookies();
+        if (Objects.isNull(cookies)) {
             throw new BackEndApplicationException(ErrorCodes.TOKEN_NULL_EXCEPTION, HttpStatus.BAD_REQUEST);
         }
 
-        validateAuthorizationFormat(authorizationHeader);
-        return authorizationHeader.substring(BEARER_TYPE.length()).trim();
+        final Cookie authCookie = getAuthorizationCookie(cookies);
+        if (Objects.isNull(authCookie)) {
+            throw new BackEndApplicationException(ErrorCodes.TOKEN_NULL_EXCEPTION, HttpStatus.BAD_REQUEST);
+        }
+
+        return authCookie.getValue();
     }
 
-    private static void validateAuthorizationFormat(final String authorizationHeader) {
-        if (!authorizationHeader.toLowerCase().startsWith(BEARER_TYPE.toLowerCase())) {
-            throw new BackEndApplicationException(ErrorCodes.TOKEN_BEARER_TYPE_EXCEPTION,HttpStatus.BAD_REQUEST);
+    private static Cookie getAuthorizationCookie(final Cookie[] cookies) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(AUTH_COOKIE_NAME)) {
+                return cookie;
+            }
         }
+        return null;
     }
 }

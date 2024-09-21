e7aa52b5b813ad47f3501755d6a4305fb8686bed
yuseonjun <seonjun0906@gmail.com>
2024-09-21 17:20:44 +0900

    [Refactor] MDC 구현

--- doochul/src/main/java/org/doochul/common/interceptor/AuthInterceptor.java
+++ doochul/src/main/java/org/doochul/common/interceptor/AuthInterceptor.java
@@ -2,9 +2,12 @@ package org.doochul.common.interceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.doochul.domain.oauth.jwt.JwtProvider;
+import org.slf4j.MDC;
 import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @RequiredArgsConstructor
@@ -17,8 +20,25 @@ public class AuthInterceptor implements HandlerInterceptor {
         if (CorsUtils.isPreFlightRequest(request)) {
             return true;
         }
+        UUID uuid = UUID.randomUUID();
+        System.out.println(uuid.toString());
+        MDC.put("request_id", uuid.toString());
+
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            String controllerName = handlerMethod.getBeanType().getSimpleName();
+            String methodName = handlerMethod.getMethod().getName();
+            MDC.put("controller", controllerName);
+            MDC.put("method", methodName);
+        }
+
         final String token = AuthorizationExtractor.extract(request);
         jwtProvider.getPayload(token);
         return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        MDC.clear();
     }
 }
